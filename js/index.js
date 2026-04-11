151c1de6f51dda1b38e5178cc0b163c25bd41e15
aitageo <aitageo1980@hotmail.com>
2026-04-11 15:35:26 -0500

    el carrito muestra los productos

--- js/index.js
+++ js/index.js
@@ -1,88 +1,80 @@
-//Formas de acceder a los Elementos//
-// const carrito = document.querySelector("#carrito");//Con querySelector puedes acceder a clases y Ids
-// console.log(carrito);
-
-// const carrito = document.getElementById("carrito");
-
-// const carrito = document.getElementsByTagName('span')
-// console.log(carrito);
-
-//Eventos de javascript
-//Eventos del mouse
-//Listeners: Es poner algo 
-//Evento click
-document.addEventListener('DOMContentLoaded', ()=>{
-  const lista_carrito = document.getElementById('lista-carrito');
-  let totalCarrito = [];
+document.addEventListener("DOMContentLoaded",(e)=>{
+  const contenedor_carrito = document.getElementById('contenedor-carrito');
+  const carrito_count = document.getElementsByClassName('carrito-count')[0];
   let count = 0;
+  const carrito = document.querySelector('.carrito');
+  let Totalcarrito = [];
   const buttons = document.querySelectorAll('.btn');
-  buttons.forEach( (button)=>{
-    button.addEventListener('click', (e)=>{      
-      e.preventDefault();
-      const carrito_count = document.getElementsByClassName('carrito-count')[0];
-      console.log(carrito_count);
-      
+  buttons.forEach(button=>{
+    button.addEventListener('click',(e)=>{
       count++
-      carrito_count.textContent = count;
-      button.textContent = "Producto agregado"
+      carrito_count.textContent = count
+      button.textContent = "Producto agregado";
       button.classList.remove('btn-primary')
-      button.classList.add('btn-success')
+      button.classList.add('btn-success');
+      e.preventDefault();
+      const data = e.target.closest('.Producto')
+      leerDatos(data);
       setTimeout(()=>{
-        button.classList.remove('btn-success')
-        button.classList.add('btn-primary')  
-        button.textContent = "Agregar al carrito"
-      },2000)
-      //Esto se llama el traversing del dom
-      const data = e.target.closest('.Producto')//el closest busca la clase mas cercana      
-      leerDatos(data)
-    //  cargarProductos(data)  
+        button.textContent = "Agregar al carrito";
+        button.classList.remove('btn-success');
+        button.classList.add('btn-primary');  
+      },1000)
     })
 
-   function leerDatos(producto){
+    function leerDatos(producto){
+      const infoProducto = {
+        id : producto.querySelector('a').getAttribute('data-id'),
+        img : producto.querySelector('img').src,
+        titulo: producto.querySelector('h5').textContent,
+        precio: parseInt(producto.querySelector('p').textContent),
+        cantidad : 1
+      }
 
-    const infoProducto = {
-      imagen : producto.querySelector('img').src,
-      titulo : producto.querySelector('h5').textContent, //es el contenido del texto
-      precio : parseInt(producto.querySelector('.precio').textContent),//parseint es para convertir texto a numero
-      id : producto.querySelector('a').getAttribute('data-id'),
-      cantidad : 1,
+      const existe = Totalcarrito.some(producto=>{
+        return producto.id === infoProducto.id
+      });
+
+      if (existe) {
+        Totalcarrito = Totalcarrito.map(producto=>{
+          if (producto.id == infoProducto.id) {
+            producto.cantidad++
+          }
+          return producto; 
+        })
+      } else {
+        Totalcarrito.push(infoProducto);
+      }
+
+      MostarCarrito(Totalcarrito); 
     }
 
-    // totalCarrito.push(infoProducto);
-    const existe = totalCarrito.some(producto=>producto.id === infoProducto.id);
-    if (existe){
-      totalCarrito = totalCarrito.map((producto)=>{
-        if (producto.id === infoProducto.id) {
-          producto.cantidad++
-          // console.log(producto);
-          
-        }
-        return producto
-      })
-    }else {
-      totalCarrito.push(infoProducto);
-      console.log(totalCarrito);
+    function MostarCarrito(Totalcarrito){
+      const tbody = document.querySelector('#lista-carrito tbody');
+      const nombre = "Alfonso"
+      console.log(`Este es mi nombre ${nombre}`);
+      const total_productos = document.querySelector('#total-carrito');
+      let sumatotal = 0
+      tbody.innerHTML = "";
+      Totalcarrito.forEach(producto=>{
+        const row = document.createElement('tr');
+        let total = producto.precio * producto.cantidad
+        row.innerHTML = `
+          <td><img src="${producto.img}" width="50"></td>
+          <td>${producto.titulo}</td>
+          <td>$${total}</td>
+          <td>${producto.cantidad}</td>
+          <td><a href="#" class="eliminar" data-id="${producto.id}">X</a></td>
+        `;
 
+        console.log(sumatotal);
+        sumatotal += total;
+        total_productos.textContent = `Total: $${sumatotal}`;
+
+         
+        tbody.appendChild(row);//Agregar un hijo
+      });
     }
-    
-    
-   }
 
-  
   })
-  
-
-
-
-  
-
-})   
-
-
-
-
-
-
-
-
-
+})
